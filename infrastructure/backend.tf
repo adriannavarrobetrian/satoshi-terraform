1c80bfd02da7ace5faba111dd8ab674ba86b7b85
Adrian Navarro <adrian.navarro@visiondirect.co.uk>
2023-12-04 17:55:55 +0100

    Change bucket state naming

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-satoshi-dev"
-    key     = "terraform.tfstate"
+    # bucket  = "satoshi-terraform-state-dev"
+    # key     = "terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
   }
